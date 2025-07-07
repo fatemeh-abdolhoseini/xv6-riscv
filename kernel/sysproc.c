a49895def855df30b28bf36e4067f8ba5dac250d
fatemeh-abdolhoseini <fatemeh@example.com>
2025-07-07 19:34:39 +0330

    in the file sysproc.c
    پیادهسازی سیستمکالها در کرنل

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,29 +47,6 @@ sys_sbrk(void)
     return -1;
   return addr;
 }
-
-uint64
-sys_sleep(void)
-{
-  int n;
-  uint ticks0;
-
-  argint(0, &n);
-  if(n < 0)
-    n = 0;
-  acquire(&tickslock);
-  ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
-      release(&tickslock);
-      return -1;
-    }
-    sleep(&ticks, &tickslock);
-  }
-  release(&tickslock);
-  return 0;
-}
-
 uint64
 sys_kill(void)
 {
@@ -90,4 +67,46 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64 sys_thread(void) {
+uint64 start_thread, stack_address, arg;
+argaddr(0, &start_thread);
+argaddr(1, &stack_address);
+argaddr(2, &arg);
+struct thread *t = allocthread(start_thread, stack_address, arg);
+return t ? t->id : 0;
+}
+
+uint64 sys_jointhread(void) {
+int id;
+argint(0, &id);
+return jointhread(id);
+}
+
+
+uint64
+sys_sleep(void)
+{
+int n;
+uint ticks0;
+argint(0, &n);
+if(n < 0)
+n = 0;
+acquire(&tickslock);
+ticks0 = ticks;
+if (myproc()->current_thread) {
+release(&tickslock);
+sleepthread(n, ticks0);
+return 0;
+}
+while(ticks - ticks0 < n){
+if(killed(myproc())){
+release(&tickslock);
+return -1;
+}
+sleep(&ticks, &tickslock);
+}
+release(&tickslock);
+return 0;
 }
