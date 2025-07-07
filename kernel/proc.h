b7757e2c649d470d24e462a4a93c8a189f8d92c6
fatemeh-abdolhoseini <fatemeh@example.com>
2025-07-07 19:20:37 +0330

    in the file proc.h
    ساختار و وضعیت ترد هارو تعریف میکنیم در ساختار پراسز تغییر ایجاد میکنیم  و  توابع را تعریف میکنیم.

--- kernel/proc.h
+++ kernel/proc.h
@@ -100,6 +100,24 @@ uint sleep_tick0;
 };
 
 
+enum threadstate {
+THREAD_UNUSED,
+THREAD_RUNNABLE,
+THREAD_RUNNING,
+THREAD_JOINED,
+THREAD_SLEEPING
+};
+
+struct thread {
+enum threadstate state;
+struct trapframe *trapframe;
+uint id;
+uint join;
+int sleep_n;
+uint sleep_tick0;
+};
+
+
 // Per-process state
 struct proc {
   struct spinlock lock;
@@ -123,6 +141,12 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+   struct thread threads[NTHREAD];
+  struct thread *current_thread;
 };
 
 
+struct thread *initthread(struct proc *p);
+int thread_schd(struct proc *p);
+void freethread(struct thread *t);
